4dc1f2767b551547484fa263aafd537510e1411d
unknown <Hispster@examble.ccc>
2024-10-16 19:34:52 +0300

    Pabeigts uzdeemuvs

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ console.log('Javascript loaded!');
 const boxContainer = document.getElementById('box-container');
 const changeColorBtn = document.getElementById('change-color');
 const toggleBtn = document.getElementById('toggle-btn');
+const deleteBtn = document.getElementById('delete-box');
+const createElementBtn = document.getElementById('create-box');
 
 toggleBtn.addEventListener("click", processBtnClick);
 changeColorBtn.addEventListener("click", changeColorClick);
+deleteBtn.addEventListener('click', deleteElement);
+createElementBtn.addEventListener('click', createElement);
 
-function processBtnClick(event){
+function processBtnClick(){
     //console.log(event);
     if(boxContainer.classList.contains('hidden')){
         boxContainer.classList.remove('hidden');
@@ -21,9 +25,29 @@ function processBtnClick(event){
 
     //boxContainer.classList.toggle('hidden');
 }
-function changeColorClick(event){
-    boxContainer.classList.add('color');
+function changeColorClick(){
+    //boxContainer.classList.toggle('color');
+    boxContainer.style.backgroundColor = 'blue';
 }
+function deleteElement(){
+    boxContainer.remove();
+}
+function createElement(){
+    var newBox = document.createElement('div');
+    newBox.style.backgroundColor = 'yellow';
+    newBox.className = 'box';
+    document.getElementsByClassName("main")[0].appendChild(newBox);
+
+
+    /*
+    const mainContainer = document.querySelector('.main');
+    var newBox = document.createElement('div');
+    newBox.className = 'box';
+    newBox .style.backgroundColor = 'yellow';
+    mainContainer.appendChild(newBox);
+    */
+}
+
 
 
 /*
